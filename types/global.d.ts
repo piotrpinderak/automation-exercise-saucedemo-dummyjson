f7fa9c721164b385c51ee645e3302d0fff948abf
Piotr Pinderak <piotr.pinderak@gmail.com>
2026-01-14 11:34:58 +0100

    Remove unused files

--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-
-declare function inject(): any;
-declare function actor(customSteps?: any): any;
-
-declare const Given: any;
-declare const When: any;
-declare const Then: any;
